c61fd70a657dd2fbba48c3cb190aceb184a7365b
Alexey Diushen <dushen.alexey@gmail.com>
2023-01-29 06:53:40 +0400

    Extract date conversion to Utils

--- src/main/java/com/blacky/covid19/service/FacadeService.java
+++ src/main/java/com/blacky/covid19/service/FacadeService.java
@@ -7,15 +7,12 @@ import com.blacky.covid19.model.Country;
 import com.blacky.covid19.model.OneDayResult;
 import com.blacky.covid19.model.mapper.CountryMapper;
 import com.blacky.covid19.model.mapper.OneDayResultMapper;
+import com.blacky.covid19.util.Utils;
 import com.blacky.covid19.web.model.CalcFunction;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -24,8 +21,6 @@ import java.util.stream.Collectors;
 @Service
 public class FacadeService {
 
-    private final static LocalTime ZERO_HOUR = LocalTime.of(0, 0, 0);
-    private final static LocalTime FIRST_HOUR = LocalTime.of(1, 0, 0);
     private final Covid19ApiClient httpService;
 
     public FacadeService(Covid19ApiClient httpService) {
@@ -41,14 +36,11 @@ public class FacadeService {
     }
 
     @SneakyThrows
-    public CalculatedResult getConfirmedCasesInPeriod(CalcFunction function, LocalDate fromLocal, LocalDate toLocal, List<String> countries, String uri) {
-        ZonedDateTime fromZdt = ZonedDateTime.of(fromLocal, ZERO_HOUR, ZoneOffset.UTC);
-        ZonedDateTime toZdt = fromLocal.equals(toLocal)
-                ? ZonedDateTime.of(toLocal, FIRST_HOUR, ZoneOffset.UTC)
-                : ZonedDateTime.of(toLocal, ZERO_HOUR, ZoneOffset.UTC);
-
-        String from = fromZdt.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
-        String to = toZdt.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+    public CalculatedResult getConfirmedCasesInPeriod(CalcFunction function, LocalDate fromDate, LocalDate toDate, List<String> countries, String uri) {
+        String from = Utils.convertWithZeroHourUtc(fromDate);
+        String to = toDate.equals(fromDate) 
+                ? Utils.convertWithFirstHourUtc(toDate) 
+                : Utils.convertWithZeroHourUtc(toDate);
 
         List<List<CovidOneDayResult>> responses = new ArrayList<>();
         for (String country : countries) {
